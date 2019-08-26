5e0e4e440bf896da956bec2bdd1c4566a23d9742
irfan87 <nerve2009@yahoo.com>
2019-08-26 14:46:48 +0800

    setup db for polling application

--- pollsite/settings.py
+++ pollsite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # register polls
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
